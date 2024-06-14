dff0642b445368a1e63c2d38fe856ba957f5c56c
sys_wsl <purep@qq.com>
2024-06-14 16:20:32 +0800

    lab2 16/29 基本完成unwarp，但是还有边角情况

--- src/wrapping_integers.cc
+++ src/wrapping_integers.cc
@@ -1,4 +1,5 @@
 #include "wrapping_integers.hh"
+#include <cmath>
 
 using namespace std;
 
@@ -10,8 +11,17 @@ Wrap32 Wrap32::wrap( uint64_t n, Wrap32 zero_point )
 
 uint64_t Wrap32::unwrap( Wrap32 zero_point, uint64_t checkpoint ) const
 {
-  // Your code here.
-  (void)zero_point;
-  (void)checkpoint;
-  return {};
+  uint64_t max2_32 = (uint64_t)1 << 32;
+  uint64_t pos_0 = raw_value_ - zero_point.raw_value_;
+
+  // 找离checkpoint最近的，可以加n个1<<32
+  auto n = checkpoint / max2_32;
+  checkpoint %= max2_32;
+
+  // n或n+1，checkpoint处于(n - 1) * max2_32到n * max2_32到(n + 1) * max2_32之间
+  if ((checkpoint > pos_0) && (checkpoint - pos_0 > max2_32 / 2))
+    return pos_0 + (n + 1) * max2_32;
+  else if ((pos_0 > checkpoint) && (pos_0 - checkpoint > max2_32 / 2))
+    return pos_0 + (n - 1) * max2_32;
+  return pos_0 + n * max2_32;
 }
